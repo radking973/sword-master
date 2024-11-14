65e1137e5d242a43639f246ce8a83aa9800bf6a4
Rusty919 <michalskir25@stxtigers.com>
2024-11-14 14:00:13 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -16,5 +16,6 @@ let mySprite = sprites.create(img`
     . . . . f f . . . f f . . . . . 
     . . . . f . . . . . f . . . . . 
     `, SpriteKind.Player)
+scene.cameraFollowSprite(mySprite)
 controller.moveSprite(mySprite)
 tiles.setCurrentTilemap(tilemap`level2`)
